879eb31fb068e0ad8aee9d9a11926765b980e104
Arseniy <ambelyakov@gmail.com>
2020-11-09 23:23:41 +0300

    First commit

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 1;
+    public float speedRotation = 1;
+    private CharacterController chController;
+    void Start()
+    {
+        chController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,Input.GetAxis("Horizontal")*speedRotation, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        chController.SimpleMove(forward * curSpeed);
+    }
+}
